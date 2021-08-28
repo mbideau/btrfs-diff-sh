feb50e859eb7d1b390595892fbf1517b5248813f
Michael Bideau <mica.devel@gmail.com>
2021-08-28 13:29:17 +0200

    fix: shellcheck fail on test script

--- test.sh
+++ test.sh
@@ -40,7 +40,7 @@ set -e
 THIS_DIR="$(dirname "$(realpath "$0")")"
 
 if [ "$BTRFS_DIFF" = '' ]; then
-    if ! BTRFS_DIFF="$(which btrfs-diff 2>/dev/null)"; then
+    if ! BTRFS_DIFF="$(command -v btrfs-diff 2>/dev/null)"; then
         if [ -x "$THIS_DIR/btrfs_diff.sh" ]; then
             BTRFS_DIFF="$THIS_DIR/btrfs_diff.sh"
         else
@@ -51,7 +51,7 @@ if [ "$BTRFS_DIFF" = '' ]; then
 fi
 
 if [ "$SHUNIT2" = '' ]; then
-    if ! SHUNIT2="$(which shunit2)"; then
+    if ! SHUNIT2="$(command -v shunit2)"; then
         if [ -r "$(dirname "$THIS_DIR")/shunit2/shunit2" ]; then
             SHUNIT2="$(dirname "$THIS_DIR")/shunit2/shunit2"
         else
@@ -95,7 +95,7 @@ oneTimeSetUp()
 {
     # create the test directory
     if [ ! -d "$TEST_DIR" ]; then
-        __debug "Creating directory: '%s'\n" "$TEST_DIR"
+        __debug "Creating directory: '%s'\\n" "$TEST_DIR"
         mkdir "$TEST_DIR"
     fi
 
@@ -109,7 +109,7 @@ oneTimeTearDown()
 {
     # remove the tests dir
     if [ -d "$TEST_DIR" ]; then
-        __debug "Removing directory: '%s'\n" "$TEST_DIR"
+        __debug "Removing directory: '%s'\\n" "$TEST_DIR"
         rmdir "$TEST_DIR"
     fi
 }
@@ -117,19 +117,19 @@ oneTimeTearDown()
 # reset conf data and savepoints
 setUp()
 {
-    __debug "Creating a subvolume that will contains the data: '%s' (read-write)\n" "$DATA_DIR"
+    __debug "Creating a subvolume that will contains the data: '%s' (read-write)\\n" "$DATA_DIR"
     btrfs subvolume create "$DATA_DIR" > /dev/null
 
-    __debug "Creating a directory that will contains the snapshots: '%s'\n" "$SNAPS_DIR"
+    __debug "Creating a directory that will contains the snapshots: '%s'\\n" "$SNAPS_DIR"
     [ ! -d "$SNAPS_DIR" ] && mkdir -p "$SNAPS_DIR"
 
-    __debug "Creating first snapshot (with no data) '%s'\n" "$SNAPS_DIR"/000
+    __debug "Creating first snapshot (with no data) '%s'\\n" "$SNAPS_DIR"/000
     btrfs subvolume snapshot -r "$DATA_DIR" "$SNAPS_DIR"/000 > /dev/null
 }
 
 tearDown()
 {
-    __debug "Removing existing data and snapshots\n"
+    __debug "Removing existing data and snapshots\\n"
     [ ! -d "$DATA_DIR" ] || btrfs subvolume delete "$DATA_DIR" > /dev/null
     if [ -d "$SNAPS_DIR" ]; then
         if [ "$(ls "$SNAPS_DIR")" != '' ]; then
@@ -147,7 +147,7 @@ tearDown()
 # inspired by: 'btrfs-send-go' tests cases
 test__lotsOfOperationsAndDiffAnalysis()
 {
-    __debug "Creating data and snapshots for each commands\n"
+    __debug "Creating data and snapshots for each commands\\n"
     I=1
     { cat <<ENDCAT
     echo foo > foo_file
@@ -157,7 +157,7 @@ test__lotsOfOperationsAndDiffAnalysis()
     ln bar/baz_file bar/baaz_file
     mv bar/baz_file bar/foo_file
     rm bar/foo_file
-    echo super_ugly_filename > cec]i[est-une\`hor#rible|ch.enne&de@charactère}q{ui~\$'assume)d(epuis*longtemps\sinon+voir^encore?.txt
+    echo super_ugly_filename > cec]i[est-une\`hor#rible|ch.enne&de@charactère}q{ui~\$'assume)d(epuis*longtemps\\sinon+voir^encore?.txt
     rm -rf bar
     mkdir dir
     touch dir/file
@@ -172,15 +172,16 @@ test__lotsOfOperationsAndDiffAnalysis()
 ENDCAT
 } | grep -v '^\s*#' | while read -r command; do
         __debug '%d: %s\n' "$I" "$command"
-        cd "$DATA_DIR"
-        eval "$(escape_eval "$command")"
-        cd - >/dev/null
+        (
+            cd "$DATA_DIR"
+            eval "$(escape_eval "$command")"
+        )
         btrfs subvolume snapshot -r "$DATA_DIR" "$SNAPS_DIR/$(printf "%03i" $I)" > /dev/null
         I="$((I + 1))"
     done
 
     __debug "Comparing snapshots between them with '%s' "`
-            `"then with '%s' and printing unmatching lines (between both diffs)\n" \
+            `"then with '%s' and printing unmatching lines (between both diffs)\\n" \
             "$BTRFS_DIFF" 'diff'
 
     # looping twice on snapshots
@@ -192,13 +193,13 @@ ENDCAT
 
             [ "$DEBUG_TEST" = 'true' ] || printf '.'
 
-            __debug "Comparing snapshots '%s' and '%s'\n" \
-                "$(echo "$A" | sed "s|$TEST_DIR/\?||g")" \
-                "$(echo "$B" | sed "s|$TEST_DIR/\?||g")"
+            __debug "Comparing snapshots '%s' and '%s'\\n" \
+                "$(echo "$A" | sed "s|$TEST_DIR/\\?||g")" \
+                "$(echo "$B" | sed "s|$TEST_DIR/\\?||g")"
 
             # produce a diff using the 'btrfs-diff' binary
             LC_ALL=C "$BTRFS_DIFF" -d "$A" "$B" >/tmp/out.diff 2>&1 || true
-            __debug "Result:\n---\n%s\n---\n" "$(cat /tmp/out.diff)"
+            __debug "Result:\\n---\\n%s\\n---\\n" "$(cat /tmp/out.diff)"
 
             # diff the diffs between them and report differences
             assertContains "$(cat /tmp/out.diff)" \
